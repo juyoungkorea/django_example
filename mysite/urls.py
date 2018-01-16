3cfd0be2ad402550648faba5ca64b9ca55f87538
juyoung <juyoungkorea@gmail.com>
2018-01-17 00:01:04 +0900

    add index.html and nodify urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    from django.conf.urls.static import static
+
+    urlpatterns += [
+            path('__debug__/', include(debug_toolbar.urls)),
+            ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
